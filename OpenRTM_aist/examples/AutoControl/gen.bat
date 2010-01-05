34ed6297cfe976011e1dbb67db62eddc140c0c5c
kurihara <openrtm@openrtm.org>
2010-01-05 05:31:00 +0000

    RELEASE_1_0_0_RC1_p1 tagged. (r117)
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Python/tags/RELEASE_1_0_0_RC1_p1@177 3ceb7c45-664a-4a73-a5f3-5d010aa2b7c3

--- OpenRTM_aist/examples/AutoControl/gen.bat
+++
@@ -1,7 +0,0 @@
-python %PYTHONPATH%\OpenRTM_aist\rtc-template\rtc-template.py -bpython^
- --module-name=AutoControl --module-desc="Auto controller component for MobileRobot"^
- --module-version=1.0.0 --module-vendor=AIST --module-category=example^
- --module-comp-type=DataFlowComponent --module-act-type=PERIODIC^
- --module-max-inst=1^
- --inport=sens:TimedFloatSeq^
- --outport=vel:TimedFloatSeq
