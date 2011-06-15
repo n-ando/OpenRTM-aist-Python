45ea46d5b846fa5fe651fa626dca7816aa1c3156
kurihara <openrtm@openrtm.org>
2011-06-15 05:09:32 +0000

    OpenRTM-aist-Python-1.0.1 release tag.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Python/tags/RELEASE_1_0_1@408 3ceb7c45-664a-4a73-a5f3-5d010aa2b7c3

---
+++ OpenRTM_aist/examples/Slider_and_Motor/run.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- Python -*-
+
+#
+# @file run.py
+# @brief ExtTrigger example startup script
+# @date $Date: 2007/10/26 $
+#
+# Copyright (c) 2003-2007 Noriaki Ando <n-ando@aist.go.jp> and Shinji Kurihara
+#          Task-intelligence Research Group,
+#          Intelligent System Research Institute,
+#          National Institute of Industrial Science (AIST), Japan
+#          All rights reserved.
+#
+
+import sys,os,platform
+import time
+import commands
+
+nsport="2809"
+sysinfo = platform.uname()
+hostname= sysinfo[1]
+plat=sys.platform
+
+if plat == "win32":
+    os.system("start python ..\\..\\..\\bin\\rtm-naming.py")
+    os.system("start python SliderComp.py")
+    os.system("start python TkMotorComp.py")
+    time.sleep(2)
+    os.system("python Connector.py")
+
+else:
+    status,term=commands.getstatusoutput("which kterm")
+    if status != 0:
+        status,term=commands.getstatusoutput("which xterm")
+
+    if status != 0:
+        status,term=commands.getstatusoutput("which uxterm")
+
+    if status != 0:
+        status,term=commands.getstatusoutput("which gnome-terminal")
+
+    if status != 0:
+        print "No terminal program (kterm/xterm/gnome-terminal) exists."
+        exit
+
+    path = None
+    for p in sys.path:
+        if os.path.exists(os.path.join(p,"OpenRTM_aist")):
+            path = os.path.join(p,"OpenRTM_aist","utils","rtm-naming")
+            break
+    if path is None:
+        print "rtm-naming directory not exist."
+        sys.exit(0)
+
+    os.system('python %s/rtm-naming.py &'%path)
+    os.system('%s -e python SliderComp.py &'%term)
+    os.system('%s -e python TkMotorComp.py &'%term)
+    time.sleep(3)
+    os.system("python Connector.py")
