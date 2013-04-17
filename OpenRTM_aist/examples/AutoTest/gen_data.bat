fdfe4f4e51d4e6c10d5ea677dd75f690a2ba2326
n-ando <Noriaki.Ando@gmail.com>
2013-04-17 07:04:46 +0000

    branch is now closed
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Python/branches/RELENG_1_1_RC2@552 3ceb7c45-664a-4a73-a5f3-5d010aa2b7c3

--- OpenRTM_aist/examples/AutoTest/gen_data.bat
+++
@@ -1,6 +0,0 @@
-for /L %%i in (0,1,15) do (
-	echo %%i>>original-data-win
-	echo %RANDOM% %RANDOM% %RANDOM% %RANDOM% %RANDOM%>>original-data-win
-	echo message%%i>>original-data-win
-	echo.>>original-data-win
-)
