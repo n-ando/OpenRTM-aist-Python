9a4e7d7cc6982c2806a56510e028c24e7adc9895
Nobu19800 <openrtm@openrtm.org>
2018-03-22 00:08:03 +0000

    [compat,bugfix,->RELENG_1_2] fixed bug.
    git-svn-id: https://tmp.openrtm.org/svn/OpenRTM-aist-Python/trunk@963 3ceb7c45-664a-4a73-a5f3-5d010aa2b7c3

--- OpenRTM_aist/examples/SimpleIO/ConsoleOut.py
+++ OpenRTM_aist/examples/SimpleIO/ConsoleOut.py
@@ -107,7 +107,7 @@ class ConsoleOut(OpenRTM_aist.DataFlowComponentBase):
 
   def onExecute(self, ec_id):
 
-    if self._inport.isNew() or True:
+    if self._inport.isNew():
       data = self._inport.read()
       print("Received: ", data)
       print("Received: ", data.data)
